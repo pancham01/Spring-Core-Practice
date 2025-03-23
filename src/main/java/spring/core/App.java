02b980f7dc42055ae89ad7502e36933417019e2d
pancham01 <pancham9557@gmail.com>
2025-03-23 18:40:00 +0530

    update

--- src/main/java/spring/core/App.java
+++ src/main/java/spring/core/App.java
@@ -6,15 +6,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import spring.core.model.Employee;
 
 public class App {
-
 	public static void main(String[] args) {
 		ApplicationContext ioc =new AnnotationConfigApplicationContext("spring.core");
 		Employee bean1 = ioc.getBean(Employee.class);
 		bean1.setName("Vikas");
 		System.out.println(bean1);
 		
-		
-
 	}
 
 }
